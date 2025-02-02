9d79848d1bd2acc9a7058c8b16e466a327ae8154
Jakie <380686356@qq.com>
2025-02-02 16:35:11 +0800

    first commit

---
+++ Source/GodOfWarrior_C/GodOfWarrior_C.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GodOfWarrior_C.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GodOfWarrior_C, "GodOfWarrior_C" );
