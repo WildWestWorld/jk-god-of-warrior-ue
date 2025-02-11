20cb4ce1ba6455572e5b2fec71880eab773494da
Jakie <380686356@qq.com>
2025-02-11 13:32:01 +0800

    feat:完成Idle动画切换

--- Source/GodOfWarrior_C/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/GodOfWarrior_C/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,41 @@
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
 
+#include "Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	//OwningCharacter 来自于父级 UWarriorCharacterAnimInstance 在 init生命周期赋值
+	if (OwningCharacter)
+	{
+		//将OwningCharacter 转化为 AWarriorHeroCharacter 
+		// 为什么可以转化?AWarriorHeroCharacter 使我们自定义的角色基类 继承自 AWarriorBaseCharacter
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	//如果有加速度就重置Idle时间，以及进入Relax的状态，没有就计算Idle时间，时间到了 就改变Relax 状态的过渡条件
+	if (IsHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		IsShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime = IdleElapsedTime + DeltaSeconds;
+		if (IdleElapsedTime >= EnterRelaxStateThresholdTime)
+		{
+			IsShouldEnterRelaxState = true;
+		}
+		else
+		{
+			IsShouldEnterRelaxState = false;
+		}
+	}
+}
--- Source/GodOfWarrior_C/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/GodOfWarrior_C/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,31 @@ UCLASS()
 class GODOFWARRIOR_C_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	//生命周期 初始化动画
+	virtual void NativeInitializeAnimation() override;
+	//Lyra开始的生命周期
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	// VisibleDefaultsOnly: 在编辑器 只能查看,不能编辑
+	// EditDefaultsOnly:  在编辑器  可以查看也可以编辑
+
+
+	// AWarriorHeroCharacter 使我们自定义的角色基类 继承自 AWarriorBaseCharacter
+	//用于获取英雄角色的实例，AWarriorBaseCharacter = 泛化  AWarriorHeroCharacter = 具体，就是获取Hero角色的具体信息
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|References")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	//是否进入Relax 状态
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	bool IsShouldEnterRelaxState;
+
+	//进入Relax事件门槛时间
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="AnimData|LocomotionData")
+	float EnterRelaxStateThresholdTime = 5.f;
+
+	//记录角色不动/Idle的时间
+	float IdleElapsedTime = 0.f;
 };
