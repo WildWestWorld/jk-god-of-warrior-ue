7c0e1ec648da1d74bc22cb9d2ac37ce4a4119327
Jakie <380686356@qq.com>
2025-03-09 17:30:53 +0800

    feat:初始化 Master Anim Layer

--- Source/GodOfWarrior_C/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
+++ Source/GodOfWarrior_C/Private/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.cpp
@@ -3,3 +3,16 @@
 
 #include "AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h"
 
+/**
+ * 获取英雄角色动画实例
+ * 该函数通过以下步骤获取动画实例:
+ * 1. 使用GetOwningComponent()获取拥有该动画层的骨骼网格体组件
+ * 2. 调用GetAnimInstance()获取骨骼网格体当前的动画实例
+ * 3. 使用Cast将获取的动画实例转换为UWarriorHeroAnimInstance类型
+ * 
+ * @return 返回转换后的英雄角色动画实例指针，如果转换失败则返回nullptr
+ */
+UWarriorHeroAnimInstance* UWarriorHeroLinkedAnimLayer::GetHeroAnimInstance() const
+{
+	return Cast<UWarriorHeroAnimInstance>(GetOwningComponent()->GetAnimInstance());
+}
--- Source/GodOfWarrior_C/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
+++ Source/GodOfWarrior_C/Public/AnimInstances/Hero/WarriorHeroLinkedAnimLayer.h
@@ -3,15 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "WarriorHeroAnimInstance.h"
 #include "AnimInstances/WarriorBaseAnimInstance.h"
 #include "WarriorHeroLinkedAnimLayer.generated.h"
 
 /**
- * 
+ * 英雄角色动画链接层类
+ * 该类用于连接和管理英雄角色的动画实例
+ * 继承自基础动画实例类 UWarriorBaseAnimInstance
  */
 UCLASS()
 class GODOFWARRIOR_C_API UWarriorHeroLinkedAnimLayer : public UWarriorBaseAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	/**
+	 * 获取英雄角色动画实例的方法
+	 * @return 返回英雄角色动画实例的指针
+	 * @note 该函数在蓝图中是纯函数，是线程安全的
+	 */
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	UWarriorHeroAnimInstance* GetHeroAnimInstance() const;
 };
